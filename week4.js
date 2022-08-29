a8d9db438ded3405f6abf07dbae627df6fe247a8
bianca-schulze <100173043+bianca-schulze@users.noreply.github.com>
2022-08-29 13:47:20 +1000

    Logo complete

--- week4.js
+++ week4.js
@@ -3,8 +3,24 @@ function setup() {
 }
 
 function draw() {
-    background(220);
+    background(149, 206, 147);
 
-    rect(50, 50, 500, 50)
-    triangle(30, 75, 58, 20, 86, 75);
+    noStroke();
+    circle(140, 175, 150)
+    circle(140, 325, 150);
+    fill('rgba(255,255,255, 0.50)')
+    circle(135, 170, 150)
+    circle(135, 320, 150);
+    fill('rgba(255,255,255, 0.25)')
+    circle(130, 165, 150)
+    circle(130, 315, 150);
+
+    fill('white');
+    textSize(65);
+    text('Bianca', 245, 230);
+
+    text('Design', 245, 305);
+    textFont('Bodini');
+
+    describe('canvas with  mint green background');
 }
