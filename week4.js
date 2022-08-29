a2148533bddb494b390a0d75b694d83907466932
bianca-schulze <100173043+bianca-schulze@users.noreply.github.com>
2022-08-29 14:32:42 +1000

    Final update to the logo - week 4 designs

--- week4.js
+++ week4.js
@@ -5,22 +5,42 @@ function setup() {
 function draw() {
     background(149, 206, 147);
 
-    noStroke();
-    circle(140, 175, 150)
-    circle(140, 325, 150);
-    fill('rgba(255,255,255, 0.50)')
-    circle(135, 170, 150)
-    circle(135, 320, 150);
-    fill('rgba(255,255,255, 0.25)')
-    circle(130, 165, 150)
-    circle(130, 315, 150);
+    let midx = 250;
+    let midy = 250;
 
+
+
+    //circle one
+    noStroke();
+    circle(midx - 110, midy - 75, 150)
+    circle(midx - 110, midy + 75, 150);
+    //circle two
+    fill(255, 128)
+    circle(midx - 115, midy - 80, 150)
+    circle(midx - 115, midy + 70, 150);
+    //circle three
+    fill(255, 64)
+    circle(midx - 120, midy - 85, 150)
+    circle(midx - 120, midy + 65, 150);
+
+    //text
     fill('white');
     textSize(65);
-    text('Bianca', 245, 230);
-
-    text('Design', 245, 305);
+    text('Bianca', midx - 5, midy - 20);
+    text('Design', midx - 5, midy + 55);
     textFont('Bodini');
 
-    describe('canvas with  mint green background');
+    //description
+    describe('canvas with  mint green background and text saying Bianca Design');
 }
+
+//function logoB(cx, cy, radius) {
+//
+//    //circle one
+//    noStroke();
+//    circle(cx - 110, cy - 75, 150) circle(cx - 110, cy + 75, 150);
+//    //circle two
+//    fill(255, 128) circle(cx - 115, cy - 80, 150) circle(cx - 115, cy + 70, 150);
+//    //circle three
+//    fill(255, 64) circle(cx - 120, cy - 85, 150) circle(cx - 120, cy + 65, 150);)
+//}
