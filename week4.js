adad7ed7f40eb63bcd01e6affe998b41343af664
bianca-schulze <100173043+bianca-schulze@users.noreply.github.com>
2022-08-29 12:28:24 +1000

    First few changes

--- week4.js
+++ week4.js
@@ -1,5 +1,10 @@
-
 function setup() {
-  createCanvas(500, 500);
+    createCanvas(500, 500);
 }
 
+function draw() {
+    background(220);
+
+    rect(50, 50, 500, 50)
+    triangle(30, 75, 58, 20, 86, 75);
+}
